8a4466e78e411ca0f4e45188e5a9443ca23af603
Eden Chambers <EdenChambers5@gmail.com>
2019-01-28 13:29:31 -0700

    I made the project and the controllers & runners inside.
    Basic structure done, but it does nothing right now.
    Also made the gitignore.

---
+++ VectorProject/Controller/Controller.hpp
@@ -0,0 +1,21 @@
+//
+//  Controller.hpp
+//  VectorProject
+//
+//  Created by Chambers, Eden on 1/28/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#ifndef Controller_hpp
+#define Controller_hpp
+
+#include <iostream>
+using namespace std;
+
+class Controller
+{
+public:
+    void start();
+};
+
+#endif /* Controller_hpp */
---
+++ VectorProject/Controller/Runner.cpp
@@ -0,0 +1,18 @@
+//
+//  Runner.cpp
+//  VectorProject
+//
+//  Created by Chambers, Eden on 1/28/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#include "Controller.hpp"
+
+int main()
+{
+    Controller * app = new Controller();
+    app->start();
+    
+    return 0;
+}
+
